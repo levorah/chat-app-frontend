7bed87c0331d9c8e990a3a77ec092611defab379
levorah <levora004@gmail.com>
2025-03-09 21:46:03 +0000

    feat: we create each routes for our app such as home signup and login and we use link to hyprelink the path

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { Route, Routes } from "react-router-dom"
 import Home from "./pages/home/home"
+import Login from "./pages/login/login"
+import SignUp from "./pages/signup/signup"
 
 function App() {
 
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Home />
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   )
 }
