03e5717354ac01b78f48203719c71a8058e29da3
levorah <levora004@gmail.com>
2025-03-09 21:30:41 +0000

    feat: we added message input container and messages and message for our app

---
+++ src/components/messages/message.tsx
@@ -0,0 +1,18 @@
+
+const Message = () => {
+    return (
+        <div className="chat chat-end">
+            <div className="chat-image avatar">
+                <div className="w-10 rounded-full">
+                    <img
+                        alt="Tailwind CSS chat bubble component"
+                        src={'https://hehe'} />
+                </div>
+            </div>
+            <div className="chat-bubble text-white bg-blue-500">Hi Whatsup</div>
+            <div className="chat-footer text-white bg-blue-500">12.23</div>
+        </div>
+    )
+}
+
+export default Message
