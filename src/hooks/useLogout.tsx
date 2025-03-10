91023ecceaa483fc0f56ad42fa119759ab1d24e4
levorah <levora004@gmail.com>
2025-03-10 12:24:14 +0000

    feat: we create a logout button hook to make lgoout the user we created custom hooks to make code more modular

---
+++ src/hooks/useLogout.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import toast from "react-hot-toast"
+import { useAuthContext } from "../context/authContext"
+
+const useLogout = () => {
+    const { setAuthUser } = useAuthContext()
+    const [loading, setLoading] = useState(false)
+    const logout = async () => {
+        try {
+            const res = await fetch(`/v1/api/auth/logout`)
+            const data = await res.json()
+            if (data.error) {
+                throw new Error(data.error)
+            }
+
+            localStorage.removeItem("chat-user")
+            setAuthUser(null)
+        } catch (error: any) {
+            toast.error(error.message)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return { loading, logout }
+}
+
+export default useLogout
