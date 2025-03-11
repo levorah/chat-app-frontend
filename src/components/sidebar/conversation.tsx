5e5693d54dc14cec68eec5b77e022d844c0a2882
levorah <levora004@gmail.com>
2025-03-11 21:05:52 +0000

    fix: we resolved some issue regarding socket io when it showing online and offline instanitvely becuase of some issue reagardin backend

--- src/components/sidebar/conversation.tsx
+++ src/components/sidebar/conversation.tsx
@@ -1,11 +1,15 @@
+import { useSocketContext } from "../../context/socketContext";
 import { User } from "../../interface";
 import useConversation from "../../zustand/useConversation";
 
 const Conversation = ({ conversation, lastIdx, emoji }: { conversation: User, lastIdx: boolean, emoji: string }) => {
 
     const { selectedConversation, setSelectedConversation } = useConversation()
-
+    const { onlineUsers } = useSocketContext()
     const isSelected = selectedConversation?._id === conversation._id
+    const isOnline = onlineUsers.includes(conversation._id)
+    console.log(conversation._id,'conversation id ')
+    console.log(isOnline, 'is online is showing')
     return (
         <>
             <div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
@@ -13,7 +17,7 @@ const Conversation = ({ conversation, lastIdx, emoji }: { conversation: User, la
                 `}
                 onClick={() => setSelectedConversation(conversation)}
             >
-                <div className='avatar online'>
+                <div className={`avatar ${isOnline ? "avatar-online" : ""}`}>
                     <div className='w-12 rounded-full'>
                         <img
                             src={conversation.profilePic}
---
+++ src/context/socketContext.tsx
@@ -0,0 +1,56 @@
+import React, { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { useAuthContext } from "./authContext";
+import { io, Socket } from "socket.io-client";
+import { DefaultEventsMap } from "socket.io";
+
+type SocketProps = {
+    children: ReactNode
+}
+type SocketType = Socket<DefaultEventsMap, DefaultEventsMap> | null
+type SocketContextType = {
+    socket: SocketType | null,
+    onlineUsers: string[],
+}
+const SocketContext = createContext<SocketContextType | undefined>(undefined)
+
+export const useSocketContext = () => {
+    const context = useContext(SocketContext)
+    if (!context) throw new Error("Socket Context Within a Socket provider")
+    return context
+}
+
+export const SocketContextProvider = ({ children }: SocketProps) => {
+    const [socket, setSocket] = useState<SocketType>(null)
+    const { authToken } = useAuthContext()
+    const [onlineUsers, setOnlineUsers] = useState([])
+    const userId = localStorage.getItem("userId")
+    console.log(userId,'user from localStorage in socket context')
+    useEffect(() => {
+        if (authToken) {
+            const socket = io(`http://localhost:3001`, {
+                query: {
+                    userId: userId
+                }
+            });
+            setSocket(socket);
+
+            socket.on("getOnlineUsers", (users) => {
+                console.log(users, 'users are hear')
+                setOnlineUsers(users)
+            })
+            return () => {
+                socket.close();
+            }
+        } else {
+            if (socket) {
+                socket.close()
+                setSocket(null)
+            }
+        }
+    }, [authToken])
+    return (
+        <SocketContext.Provider value={{ socket, onlineUsers }}>
+            {children}
+        </SocketContext.Provider>
+    )
+}
